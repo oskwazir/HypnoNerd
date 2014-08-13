cae2b4b815c4ea8343387d26582294ac5ea02c8e
Omer Wazir <omer.wazir@gmail.com>
2014-08-13 09:10:00 -0700

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Omer Wazir on 8/13/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
