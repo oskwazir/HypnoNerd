a04a46b8f06faff9e8313629e14da125bf10db3f
Omer Wazir <omer.wazir@gmail.com>
2014-08-14 08:46:30 -0700

    Add local notification

--- objective-c/HypnoNerd/BNRReminderViewController.m
+++ objective-c/HypnoNerd/BNRReminderViewController.m
@@ -41,6 +41,12 @@
 
     NSDate *date = self.datePicker.date;
     NSLog(@"Setting a reminder for %@", date);
+    
+    UILocalNotification *note = [[UILocalNotification alloc] init];
+    note.alertBody = @"Hypnotize me!";
+    note.fireDate = date;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:note];
 }
 
 @end
