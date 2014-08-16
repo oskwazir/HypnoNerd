10bda21767235f657b47e6f6c2588ad02f92103e
Omer Wazir <omer.wazir@gmail.com>
2014-08-16 13:07:34 -0700

    Add view controller for Hypnosis view

---
+++ swift/HypnoNerd/HypnoNerd/HypnosisViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HypnosisViewController.swift
+//  HypnoNerd
+//
+//  Created by Omer Wazir on 8/16/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+import UIKit
+
+class HypnosisViewController: UIViewController {
+    
+    
+    override func loadView() {
+        //create a view
+        let frame:CGRect = UIScreen.mainScreen().bounds;
+        let backgroundView:HypnosisView = HypnosisView(frame: frame);
+        
+        //set backgroundView as the view of the view controller
+        self.view = backgroundView;
+    }
+   
+}
